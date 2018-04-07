4a309a073f5b67bf60a08c773938a27b62c9a2e1
Vadim Rakevich <vrokez@gmail.com>
2018-04-08 00:30:47 +0300

    First lab now works correctly in DOSBox

--- Lab1/Lab1.asm
+++ Lab1/Lab1.asm
@@ -1,15 +1,14 @@
-.model tiny
-.code 
-org 100h 
-mov cx, 2          
-procHelloWorld:  
-       mov ah, 9
-       mov dx, offset msgHello
-       int 21h                   
-       mov dx, offset msgWorld  
-       int 21h 
-       loop procHelloWorld
-       ret
-msgHello db "Hello, ",'$'
-msgWorld db "World!", 0Dh, 0Ah, '$'
-       end procHelloWorld    
+.MODEL SMALL
+.STACK 100h
+.DATA
+    HelloMessage DB 'Hello, World!',13,10,'$'
+.CODE
+START:
+    mov ax,@data
+    mov ds,ax
+    mov ah,9
+    mov dx,OFFSET HelloMessage
+    int 21h
+    mov ah,4ch
+    int 21h
+END START
